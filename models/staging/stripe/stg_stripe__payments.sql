c591fecfe738a0e3b76fe2af7ae8d01826ac19f3
Andrii Budzan <andrewbudzan@gmail.com>
2025-10-01 15:32:27 +0000

    generated modes from source files; added statistic_raw source file.

--- models/staging/stripe/stg_stripe__payments.sql
+++
@@ -1,8 +0,0 @@
-    select
-        orderid as order_id,
-        paymentmethod as payment_method,
-        status,
-        amount / 100 as amount,
-        created as created_at
-
-    from {{ source('stripe', 'payment') }}
